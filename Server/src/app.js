1f8989b83ea6e68384f5a4c8c3dfb7f1659d1ac2
SamPharaon <Sampharaon@live.com>
2017-10-04 16:03:08 -0400

    finished video 1 created a register page with axios

--- Server/src/app.js
+++ Server/src/app.js
@@ -14,4 +14,10 @@ app.get('/status', (req, res) => {
   })
 })
 
+app.post('/register', (req, res) => {
+    res.send({
+        message: `Hello ${req.body.email} our user was registered! Have fun!!`
+    })
+})
+
 app.listen(process.env.PORT || 8081)
